c8af0c878dfa38ae016973e022e0a873832e0988
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-11-06 13:38:49 -0800

    added thought-routes and index. js file

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -9,3 +9,16 @@ const {
     deleteReaction
 } = require('../../controllers/thought-controller');
 
+// /api/thoughts
+router.route("/").get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:id
+router.route("/:id").get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
